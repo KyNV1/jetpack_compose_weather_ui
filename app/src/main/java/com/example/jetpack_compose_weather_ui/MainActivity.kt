04dae0096eaf696f1fb36b230fd3958f7a7934c9
KyNV1 <anhkyitfsoft@gmail.com>
2024-11-07 10:11:06 +0700

    Initial commit

---
+++ app/src/main/java/com/example/jetpack_compose_weather_ui/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.jetpack_compose_weather_ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.jetpack_compose_weather_ui.ui.theme.Jetpack_compose_weather_uiTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Jetpack_compose_weather_uiTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                   WeatherScreen(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun WeatherScreen(modifier: Modifier = Modifier) {
+
+}
